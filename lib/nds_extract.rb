2020d36c1c347f5dc80270600e22f05462c8aa3f
thomas dubiel <td1729@hotmail.com>
2019-12-02 17:38:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,7 +31,7 @@ def directors_totals(nds)
       director_total_gross += nds[name_index][:movies][movies_index][:worldwide_gross] 
       movies_index += 1
     end 
-    result["#{nds[name_index][:name]}"]: director_total_gross
+    result["#{nds[name_index][:name]}"] = director_total_gross
     name_index += 1
   end
   
